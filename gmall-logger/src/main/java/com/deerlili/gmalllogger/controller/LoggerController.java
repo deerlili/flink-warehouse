d478de7f32e9d803409b09277be5cf94ba59257f
李绪兴 <lixx@cqbanshan.com>
2022-05-31 17:45:52 +0800

    Initial commit

---
+++ gmall-logger/src/main/java/com/deerlili/gmalllogger/controller/LoggerController.java
@@ -0,0 +1,32 @@
+package com.deerlili.gmalllogger.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author deerlili
+ * @date 2022/5/31
+ * @des
+ */
+@RestController
+@Slf4j
+public class LoggerController {
+
+    @Autowired
+    private KafkaTemplate<String,String> kafkaTemplate;
+
+    @GetMapping("/applog")
+    public String applog(@RequestParam(name = "param") String param) {
+        // 数据落盘
+        log.info(param);
+        // 数据写入 Kafka
+        kafkaTemplate.send("ods_base_log",param);
+        return "success";
+    }
+
+}
