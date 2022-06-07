e3641a394bd051acf11b40437c43721841838550
李绪兴 <lixx@cqbanshan.com>
2022-06-07 14:28:56 +0800

    添加flink-cdc sql

--- gmall-cdc/src/main/java/com/deerlili/MySqlBinlogExample.java
+++ gmall-cdc/src/main/java/com/deerlili/MySqlBinlogExample.java
@@ -2,7 +2,9 @@ package com.deerlili;
 
 import com.ververica.cdc.connectors.mysql.source.MySqlSource;
 import com.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.ververica.cdc.debezium.DebeziumDeserializationSchema;
 import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
+import com.ververica.cdc.debezium.StringDebeziumDeserializationSchema;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.runtime.state.filesystem.FsStateBackend;
 import org.apache.flink.streaming.api.CheckpointingMode;
