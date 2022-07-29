41f8a17ab8b2f76f21d6741462c5486de9664436
李绪兴 <lixx@cqbanshan.com>
2022-07-29 16:39:55 +0800

    DimUtil test

--- gmall-realtime/src/main/java/com/deerlili/gmall/realtime/utils/DimUtil.java
+++ gmall-realtime/src/main/java/com/deerlili/gmall/realtime/utils/DimUtil.java
@@ -6,6 +6,7 @@ import com.deerlili.gmall.realtime.common.HbaseConfig;
 import redis.clients.jedis.Jedis;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
 
 /**
@@ -16,38 +17,22 @@ import java.util.List;
  */
 public class DimUtil {
 
-    public static JSONObject getDimInfo(Connection connection, String tableName, String id) throws Exception {
-        // 查询Phoenix之前先查Redis
-        Jedis jedis = RedisUtil.getJedis();
-        // DIM:DIM_USER_INFO:143
-        String redisKey = "DIM:" + tableName + ":" + id;
-        String dimInfo = jedis.get(redisKey);
-        if (dimInfo != null) {
-            // 重置过期时间
-            jedis.expire(redisKey, 24*60*60);
-            // 归还连接
-            jedis.close();
-            // 返回结果
-            return JSON.parseObject(dimInfo);
-        }
-
+    public static JSONObject getDimInfo(Connection connection, String tableName, String columnName,String value) throws Exception {
         // 拼接查询语句
-        String querySql = "select * from " + HbaseConfig.HBASE_SCHEMA + "." + tableName + " where id='" + id +"'";
+        String querySql = "select * from " + HbaseConfig.HBASE_SCHEMA + "." + tableName + " where " + columnName +"='"+ value +"'";
+        System.out.println(querySql);
         // 查询Phoenix
         List<JSONObject> queryList = JdbcUtil.queryList(connection, querySql, JSONObject.class, false);
         JSONObject dimInfoJson = queryList.get(0);
-        // 在返回结果之前将数据写入Redis
-        jedis.set(redisKey, dimInfoJson.toJSONString());
-        jedis.expire(redisKey, 24*60*60);
-        jedis.close();
-
         // 返回结果
         return dimInfoJson;
     }
 
-    public static void delRedisDimInfo(String tableName,String id) {
-        Jedis jedis = RedisUtil.getJedis();
-        jedis.del("DIM:" + tableName + ":" + id);
-        jedis.close();
+    public static void main(String[] args) throws Exception {
+        Class.forName(HbaseConfig.PHOENIX_DRIVER);
+        Connection connection = DriverManager.getConnection(HbaseConfig.PHOENIX_SERVER);
+        JSONObject dimInfo = getDimInfo(connection, "DIM_WARE_SKU", "ID", "29");
+        System.out.println(dimInfo);
+        connection.close();
     }
 }
