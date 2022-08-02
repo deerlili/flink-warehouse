46cb6109547ea92c341e8e992cf8f2f84d3aeccf
李绪兴 <lixx@cqbanshan.com>
2022-08-02 22:53:32 +0800

    done PaymentWideApp

--- gmall-realtime/src/main/java/com/deerlili/gmall/realtime/app/dwm/PaymentWideApp.java
+++ gmall-realtime/src/main/java/com/deerlili/gmall/realtime/app/dwm/PaymentWideApp.java
@@ -1,14 +1,23 @@
 package com.deerlili.gmall.realtime.app.dwm;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.deerlili.gmall.realtime.bean.OrderWide;
 import com.deerlili.gmall.realtime.bean.PaymentInfo;
+import com.deerlili.gmall.realtime.bean.PaymentWide;
 import com.deerlili.gmall.realtime.utils.KafkaUtil;
 import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
-import org.apache.flink.api.common.time.Time;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.ProcessJoinFunction;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 /**
  * PaymentWideApp 支付宽表程序
@@ -17,7 +26,7 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
  * @date 2022/8/2 17:59
  */
 public class PaymentWideApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //1.获取执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
@@ -41,22 +50,52 @@ public class PaymentWideApp {
         DataStreamSource<String> orderWideDS = env.addSource(KafkaUtil.getKafkaConsumer(orderWideSourceTopic, groupId));
 
         //2.1转换为JavaBean提取时间戳生成WaterMark
-        paymentInfoDS.map(line ->{
-            return JSON.parseObject(line, PaymentInfo.class);
-        }).assignTimestampsAndWatermarks(WatermarkStrategy.<PaymentInfo>forMonotonousTimestamps()
-                .withTimestampAssigner(new SerializableTimestampAssigner<PaymentInfo>() {
+        SingleOutputStreamOperator<PaymentInfo> paymentInfoDS1 = paymentInfoDS.map(line -> JSON.parseObject(line, PaymentInfo.class))
+                .assignTimestampsAndWatermarks(WatermarkStrategy.<PaymentInfo>forMonotonousTimestamps()
+                        .withTimestampAssigner(new SerializableTimestampAssigner<PaymentInfo>() {
+                            @Override
+                            public long extractTimestamp(PaymentInfo paymentInfo, long l) {
+                                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                                try {
+                                    return format.parse(paymentInfo.getCreate_time()).getTime();
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                    return l;
+                                }
+                            }
+                        }));
+        SingleOutputStreamOperator<OrderWide> orderWideDS1 = orderWideDS
+                .map(line -> JSON.parseObject(line, OrderWide.class))
+                .assignTimestampsAndWatermarks(WatermarkStrategy.<OrderWide>forMonotonousTimestamps()
+                        .withTimestampAssigner(new SerializableTimestampAssigner<OrderWide>() {
+                            @Override
+                            public long extractTimestamp(OrderWide orderWide, long l) {
+                                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                                try {
+                                    return format.parse(orderWide.getCreate_time()).getTime();
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                    return l;
+                                }
+                            }
+                        }));
+
+        //3.双流JOIN
+        SingleOutputStreamOperator<PaymentWide> paymentWideDS = paymentInfoDS1
+                .keyBy(PaymentInfo::getOrder_id)
+                .intervalJoin(orderWideDS1.keyBy(OrderWide::getOrder_id))
+                .between(Time.minutes(-15), Time.seconds(10))
+                .process(new ProcessJoinFunction<PaymentInfo, OrderWide, PaymentWide>() {
                     @Override
-                    public long extractTimestamp(PaymentInfo paymentInfo, long l) {
-                        return 0;
+                    public void processElement(PaymentInfo paymentInfo, OrderWide orderWide, ProcessJoinFunction<PaymentInfo, OrderWide, PaymentWide>.Context context, Collector<PaymentWide> collector) throws Exception {
+                        collector.collect(new PaymentWide(paymentInfo, orderWide));
                     }
                 });
-        /**
-         *
-         *
-         * 3.双流JOIN
-         * 4.将数据写入Kafka
-         * 5.启动任务
-         */
+        paymentWideDS.print("paymentWideDS>>>");
+        //4.将数据写入Kafka
+        paymentWideDS.map(JSONObject::toJSONString).addSink(KafkaUtil.getKafkaProducer(paymentWideSinkTopic));
+        //5.启动任务
+        env.execute("PaymentWideApp");
 
     }
 }
